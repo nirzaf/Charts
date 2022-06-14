12ac9d0cdc1be188858424012c3429c16fbc454e
nirzaf <mfmfazrin1986@gmail.com>
2022-06-14 23:14:27 +0530

    using Microsoft.AspNetCore.Hosting; using Microsoft.Extensions.Configuration; using Microsoft.Extensions.DependencyInjection;

--- Charts/Controllers/ChartsController.cs
+++ Charts/Controllers/ChartsController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Charts.Models;
 using Microsoft.AspNetCore.Mvc;
 
--- Charts/Models/ErrorViewModel.cs
+++ Charts/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Charts.Models
 {
     public class ErrorViewModel
